f0fbbcf7ec8584242e306782a1809046b8d7b980
Caephas <arinzeobidiegwu@gmail.com>
2021-08-01 17:59:02 +0100

    project structure

---
+++ lib/src/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // child: Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('Checking.......'),
+      //     backgroundColor: Colors.amber,
+      //   ),
+      // ),
+    );
+  }
+}
